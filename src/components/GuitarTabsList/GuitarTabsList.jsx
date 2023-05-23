7af1d535e37f3e2230c450d1d91e2cfc0cc5c1c4
Oleksandr Lytvyn <tvinlee7@gmail.com>
2023-05-23 14:07:44 +0300

    delete unused

--- src/components/GuitarTabsList/GuitarTabsList.jsx
+++ src/components/GuitarTabsList/GuitarTabsList.jsx
@@ -1,13 +1,8 @@
 import { nanoid } from 'nanoid';
-import { useSelector } from 'react-redux';
 import { StyledGuitarTabsList } from './GuitarTabsList.styled';
 import { Position } from './Position';
 
 export const GuitarTabsList = ({ chord, play, setNotes }) => {
-  // const chord2 = useSelector(state => state.chord);
-  // console.log(chord2);
-  // console.log(chord);
-
   return (
     <>
       {!chord && <div>no results</div>}
